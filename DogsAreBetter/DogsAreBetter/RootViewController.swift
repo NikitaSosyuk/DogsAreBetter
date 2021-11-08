cbf2de4df859e1d000a0301cc921322f9a007c42
Nikita Sosyuk <nikitashelov@gmail.com>
2021-11-08 22:46:47 +0300

    Real initial commit.

---
+++ DogsAreBetter/DogsAreBetter/RootViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RootViewController.swift
+//  DogsAreBetter
+//
+//  Created by Nikita Sosyuk on 08.11.2021.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .black
+    }
+}
+
---
+++ DogsAreBetter/DogsAreBetter/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  DogsAreBetter
+//
+//  Created by Nikita Sosyuk on 08.11.2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        self.window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+
+        let vc = RootViewController()
+        let navigationController = UINavigationController(rootViewController: vc)
+        navigationController.navigationBar.prefersLargeTitles = true
+
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
