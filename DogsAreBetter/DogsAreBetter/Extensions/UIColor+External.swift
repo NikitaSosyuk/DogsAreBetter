063ce7f85c77f8a846e87b26a81fe8b800723dad
Nikita Sosyuk <nikitashelov@gmail.com>
2021-11-08 23:30:39 +0300

    Add Layout.

---
+++ DogsAreBetter/DogsAreBetter/Extensions/UIColor+External.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+External.swift
+//  DogsAreBetter
+//
+//  Created by Nikita Sosyuk on 08.11.2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var moreButtonColor = UIColor(red: 255 / 255, green: 155 / 255, blue: 138 / 255, alpha: 1)
+}
